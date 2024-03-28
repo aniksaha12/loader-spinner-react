1439222f9929d5d5b8d8203d9ab69937d592c32e
Aniksaha19 <46953050+aniksaha12@users.noreply.github.com>
2024-03-28 18:32:04 +0530

    first version of my react-loader

---
+++ src/CustomLoader.js
@@ -0,0 +1,13 @@
+// CustomLoader.js
+import React from 'react';
+import './CustomLoader.css';
+
+const CustomLoader = () => {
+  return (
+    <div className="custom-loader">
+      <div className="loader-spinner"></div>
+    </div>
+  );
+};
+
+export default CustomLoader;
